bd22c202ceda5901ef617fc44ae1a6370df095be
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 19:26:29 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,12 +4,7 @@ def hello_t(array)
     yield array[i]
     i += 1 
   end
+  array
 end
 
 # call your method here!
-
-hello_t(array) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
